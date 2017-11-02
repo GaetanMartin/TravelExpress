937390a6589e0d3322ea25cc67ad97e6b9b12638
GaetanMartin <gaetan.martin1993@gmail.com>
2017-11-02 14:18:56 -0400

    Fixed active menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -11,17 +11,17 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="{{ Request::is('home') ? 'active' : '' }}"><a href="{{url('/home')}}"><i class="fa fa-home" aria-hidden="true"></i> @lang('layouts.nav_menu_home')</a></li>
-                <li class="{{ Request::is('search') ? 'active' : '' }}"><a href="{{url('/trip/search')}}"><i class="fa fa-search" aria-hidden="true"></i> @lang('layouts.nav_menu_trip_search')</a></li>
-                <li class="{{ Request::is('trip') ? 'active' : '' }}"><a href="{{url('/trip/new')}}"><i class="fa fa-plus-circle" aria-hidden="true"></i> @lang('layouts.nav_menu_trip_new')</a></li>
+                <li class="{{ Route::is('home') ? 'active' : '' }}"><a href="{{url('/home')}}"><i class="fa fa-home" aria-hidden="true"></i> @lang('layouts.nav_menu_home')</a></li>
+                <li class="{{ Route::is('search') ? 'active' : '' }}"><a href="{{url('/trip/search')}}"><i class="fa fa-search" aria-hidden="true"></i> @lang('layouts.nav_menu_trip_search')</a></li>
+                <li class="{{ Route::is('trip') ? 'active' : '' }}"><a href="{{url('/trip/new')}}"><i class="fa fa-plus-circle" aria-hidden="true"></i> @lang('layouts.nav_menu_trip_new')</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li class="{{ Request::is('contact') ? 'active' : '' }}"><a href="{{url('contact')}}"><i class="fa fa-envelope-o" aria-hidden="true"></i> @lang('layouts.nav_menu_contact')</a></li>
+                <li class="{{ Route::is('contact') ? 'active' : '' }}"><a href="{{url('contact')}}"><i class="fa fa-envelope-o" aria-hidden="true"></i> @lang('layouts.nav_menu_contact')</a></li>
             	
             	@guest
             		{{-- Visitor --}}
-            		<li><a href="{{url('/login')}}"><i class="fa fa-user" aria-hidden="true"></i> @lang('layouts.nav_menu_login')</a></li>
-                    <li><a href="{{ route('register') }}"><i class="fa fa-user-plus" aria-hidden="true"></i> @lang('layouts.nav_menu_register')</a></li>
+            		<li class="{{ Route::is('login') ? 'active' : '' }}"><a href="{{url('/login')}}"><i class="fa fa-user" aria-hidden="true"></i> @lang('layouts.nav_menu_login')</a></li>
+                    <li class="{{ Route::is('register') ? 'active' : '' }}"><a href="{{ route('register') }}"><i class="fa fa-user-plus" aria-hidden="true"></i> @lang('layouts.nav_menu_register')</a></li>
             	@else
             		{{-- Logged In --}}
             		<li><a href="{{url('/logout')}}"><i class="fa fa-power-off" aria-hidden="true"></i> @lang('layouts.nav_menu_logout')</a></li>
