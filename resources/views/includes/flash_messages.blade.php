b00fba6b90e0608eb883c20f217502f174744c50
GaetanMartin <gaetan.martin1993@gmail.com>
2017-11-08 14:14:11 -0500

    Centered Flash Messages

--- resources/views/includes/flash_messages.blade.php
+++ resources/views/includes/flash_messages.blade.php
@@ -1,12 +1,12 @@
 @if (session('status_success'))
-    <div class="alert alert-success alert-dismissable fade in">
+    <div class="alert alert-success alert-dismissable fade in text-center">
     	<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         {{ session('status_success') }}
     </div>
 @endif
 
 @if (session('status_danger'))
-    <div class="alert alert-danger alert-dismissable fade in">
+    <div class="alert alert-danger alert-dismissable fade in text-center">
     	<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
         {{ session('status_danger') }}
     </div>
