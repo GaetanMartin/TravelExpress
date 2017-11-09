8bc2de5ffdb91b3b76d5cd1724e0b53f1256f420
GaetanMartin <gaetan.martin1993@gmail.com>
2017-11-09 11:13:01 -0500

    Added sections for stylesheets and javascripts + Datatables CDN on homepage

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     @include('includes.head')
+    @yield('stylesheets')
 </head>
 <body>
 <div class="container">
@@ -18,10 +19,17 @@
 
     </div>
 
-    <footer class="row">
-        @include('includes.footer')
-    </footer>
+    @include('includes.footer')
 
 </div>
+
+<!-- JQuery 3.2.1 minified -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+<!-- Bootstrap Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+@yield('scripts')
+
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,4 +1,22 @@
 @extends('layouts.default')
+
+@section('stylesheets')
+	<!-- Datatables -->
+	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+@endsection
+
 @section('content')
-    Home Page - Hello World
-@stop
+   
+
+@endsection
+
+@section('scripts')
+	<!-- Datatables -->
+	<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#example').DataTable();
+		} );
+	</script>
+@endsection
