e6b58186b74c2a33858ba68395f63eb8d9bf4975
GaetanMartin <gaetan.martin1993@gmail.com>
2017-11-13 11:03:25 -0500

    Disable booking button when the ride is full

--- resources/views/pages/rides/show.blade.php
+++ resources/views/pages/rides/show.blade.php
@@ -12,7 +12,7 @@
 			   <li>@lang('messages.th_dest_city') : {{$ride->dest_city}}</li>
 			   <li>@lang('messages.th_price') : {{$ride->price}}</li>
 			   <li>@lang('messages.th_start_time') : {{$ride->start_time->format('d/m/Y H:i')}}</li>
-			   <li>@lang('messages.th_nb_seats') : {{$ride->nb_seats_offered}}</li>
+			   <li>@lang('messages.th_nb_seats') : {{$nbSeatsAvailable = $ride->getNbSeatsAvailable($ride->id)}} / {{$ride->nb_seats_offered}}</li>
 			   <li>@lang('messages.th_luggage_size') : {{__($ride->luggage_size)}}</li>
 			</ul>
 
@@ -37,7 +37,7 @@
 	            </div>
 			@else
 				<div class="btn-group">
-					<a href="{{ route('bookings.create', ['id' => $ride->id]) }}" class="btn btn-info" role="button"><i class="fa fa-shopping-cart" aria-hidden="true"></i> @lang('messages.book')</a>
+					<a href="{{ route('bookings.create', ['id' => $ride->id]) }}" class="btn btn-info {{ $nbSeatsAvailable > 0 ? '':'disabled' }}" role="button"><i class="fa fa-shopping-cart" aria-hidden="true"></i> @lang('messages.book')</a>
 				</div>
 			@endif
 			</div>
